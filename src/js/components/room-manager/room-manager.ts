1dfbb23b54d22d5b494362e9b7a0d0dcc83efce9
Radet 5 <r.5@unbound.services>
2022-01-17 22:07:38 -0500

    put OtherPlayer class into its own file

--- src/js/components/room-manager/room-manager.ts
+++ src/js/components/room-manager/room-manager.ts
@@ -5,45 +5,7 @@ import { VRUser } from "../user/vr-user";
 import { ScreenUser } from "../user/screen-user";
 import { PhysicalObjectsManager } from "../objects-manager/physical-objects-manager";
 import { ServerDataManager } from "../server-data-manager/server-data-manager";
-
-class OtherPlayer {
-  #userID;
-  #dolly;
-  constructor(userID: string) {
-    this.#userID = userID;
-    this.#dolly = new THREE.Group();
-
-    let hex, i;
-    let result = "";
-    for (i = 0; i < userID.length; i++) {
-      hex = userID.charCodeAt(i).toString(16);
-      result += hex.slice(-4);
-    }
-    const color = "#" + result.substring(0, 6);
-
-    const boxgeometry = new THREE.BoxGeometry();
-    const boxMaterial = new THREE.MeshBasicMaterial({ color });
-    const boxMesh = new THREE.Mesh(boxgeometry, boxMaterial);
-    boxMesh.position.set(0, 0.5, 0);
-    boxMesh.scale.set(0.5, 1, 0.2);
-    this.#dolly.add(boxMesh);
-  }
-
-  addToScene(scene: THREE.Scene) {
-    scene.add(this.#dolly);
-  }
-
-  removeFromScene(scene: THREE.Scene) {
-    this.#dolly.children.forEach(child => {
-      scene.remove(child);
-    });
-    scene.remove(this.#dolly);
-  }
-
-  setPosition(position: {x: number, y: number, z: number}) {
-    this.#dolly.position.set(position.x, position.y, position.z);
-  }
-}
+import { OtherPlayer } from "../agent/other-player";
 
 export class RoomManager {
   #camera;
